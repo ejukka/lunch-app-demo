27946c3abdcbe2a4b70205e436e97b4ab517a491
Jukka Edvardsson <jukka.edvardsson@wunderdog.fi>
2017-11-23 23:02:15 +0200

    Added simple comment block with test data

--- LunchApp.Web/wwwroot/js/commentBox.jsx
+++ LunchApp.Web/wwwroot/js/commentBox.jsx
@@ -1,8 +1,21 @@
+var data = [
+    { id: 1, author: "Jedi", text: "The Force is strong with this one" },
+    { id: 2, author: "Darth Vader", text: "Impressive. Most impressive. Obi-Wan has taught you well." },
+    { id: 3, author: "Luke", text: "I hope so for your sake, the Emperor is not as forgiving as I am" }
+];
+
 var CommentList = React.createClass({
     render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-                This is commentlist.
+                {commentNodes}
             </div>
         );
     }
@@ -23,13 +36,32 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
     }
 });
+
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={data} />,
     document.getElementById('content')
 );
